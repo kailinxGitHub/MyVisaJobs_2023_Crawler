4226253e76c8aaf03b5ffc7342354ebc5ca21e2a
Kailin <kailinx@kailinx.com>
2023-04-12 23:29:32 +0200

    Update H1B_university_hires.py

--- H1B_university_hires.py
+++ H1B_university_hires.py
@@ -123,42 +123,6 @@ def get_university_data():
 
 
 def main():
-    # st.set_page_config(page_title="University Recruitment Info", layout="wide")
-    # st.title("University Recruitment Info")
-
-    # if "search_started" not in st.session_state:
-    #     st.session_state.search_started = False
-
-    # university_data = get_university_data()
-
-    # st.header("Reset Data")
-    # reset_button = st.button("Reset")
-    # run_button = st.button("Run")
-
-    # if reset_button:
-    #     csv_folder = 'csv'
-    #     for filename in os.listdir(csv_folder):
-    #         if filename.endswith('.csv'):
-    #             os.remove(f'{csv_folder}/{filename}')
-    #     st.success("All CSV files have been cleared.")
-    #     st.session_state.search_started = False
-
-    # if run_button:
-    #     st.session_state.search_started = True
-
-    # if st.session_state.search_started:
-    #     filter_company_placeholder = st.empty()
-    #     filter_company_placeholder.header("Filter by Company")
-    #     company_name = st.selectbox("Select a company", sorted(university_data['Company'].unique()))
-    #     filtered_data = university_data[university_data['Company'] == company_name]
-    #     filter_company_placeholder.write(filtered_data)
-
-    #     filter_university_placeholder = st.empty()
-    #     filter_university_placeholder.header("Filter by University")
-    #     university_name = st.selectbox("Select a university", sorted(university_data['University'].unique()))
-    #     filtered_data_by_university = university_data[university_data['University'] == university_name]
-    #     filter_university_placeholder.write(filtered_data_by_university)
-
     st.set_page_config(page_title="University Recruitment Info", layout="wide")
     st.title("University Recruitment Info")
     st.header("Reset Data")
@@ -188,6 +152,5 @@ def main():
 
     if run_button:
         start()
-
 
 main()

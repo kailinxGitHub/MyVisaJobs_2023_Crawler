b586d2ae93415ee1bbd5a4d22a56437d19b23d97
kailinxGitHub <67965369+kailinxGitHub@users.noreply.github.com>
2023-04-12 23:09:19 +0200

    commit

--- H1B_university_hires.py
+++ H1B_university_hires.py
@@ -2,10 +2,10 @@ import streamlit as st
 import pandas as pd
 import requests
 from bs4 import BeautifulSoup
-import shutil
 import re
 import csv
 import os
+import shutil
 
 def save_company_data_to_csv(name, organized_list):
     if not os.path.exists('csv'):
@@ -38,30 +38,14 @@ def load_all_company_data():
 
 def get_university_data():
     # Check if the CSV folder already exists and has files, and load the data from the files.
-    if os.path.exists('csv') and len(os.listdir('csv')) > 0:
+    if os.path.exists('csv') and any(file.endswith('.csv') for file in os.listdir('csv')):
         return load_all_company_data()
-
-    # If the CSV folder does not exist or is empty, scrape the data and save it to the files.
-    # page = requests.get('https://www.myvisajobs.com/Software-Engineer-2023JT.htm')
-    # soup = BeautifulSoup(page.text, 'html.parser')
-    # table = soup.find('table', class_='tbl')
-    # rows = table.find_all('tr')
-    # name_link_dict = {}
-    # for row in rows:
-    #     td_tags = row.find_all('td')
-    #     if len(td_tags) >= 2:
-    #         name = td_tags[1].get_text().strip()
-    #         # link
-    #         first_part = 'https://www.myvisajobs.com/'
-    #         second_part = row.find('a')['href']
-    #         link = first_part + second_part
-    #         name_link_dict[name] = link
     
     base_url = 'https://www.myvisajobs.com/Software-Engineer'
     url_suffix = '-2023JT.htm'
     name_link_dict = {}
 
-# Loop through the desired number of pages (1 to 4 in this case)
+    # Loop through the desired number of pages (1 to 4 in this case)
     for page_number in range(1, 5):
         # Generate the URL for the current page
         if page_number == 1:
@@ -112,11 +96,20 @@ def get_university_data():
             letters_to_remove = "()"
             for letter in letters_to_remove:
                 item = item.replace(letter, "")
-            name_pattern = re.compile(r"[a-zA-Z\s]+") # todo fix symbols being cut out
-            uni_name = name_pattern.search(item).group()
+            name_pattern = re.compile(r"[a-zA-Z\s]+")
+            uni_name_match = name_pattern.search(item)
+            if uni_name_match:
+                uni_name = uni_name_match.group()
+            else:
+                continue
             number_pattern = re.compile(r"\d+")
-            number = number_pattern.search(item).group()
+            number_match = number_pattern.search(item)
+            if number_match:
+                number = number_match.group()
+            else:
+                continue
             organized_list[uni_name] = number
+
 
                     # Save the company's data to a CSV file.
         save_company_data_to_csv(name, organized_list)
@@ -128,27 +121,38 @@ def get_university_data():
     # Save the data to CSV files and return the data as a pandas DataFrame.
     return pd.DataFrame(data, columns=['Company', 'University', 'Number'])
 
+# def main():
+#     st.set_page_config(page_title="University Recruitment Info", layout="wide")
+#     st.title("University Recruitment Info")
+
+#     st.header("Companies and Universities")
+
+#     university_data = get_university_data()
+#     st.write(university_data)
+
+#     st.header("Filter by Company")
+#     company_name = st.selectbox("Select a company", sorted(university_data['Company'].unique()))
+#     filtered_data = university_data[university_data['Company'] == company_name]
+#     st.write(filtered_data)
+
+#     st.header("Filter by University")
+#     university_name = st.selectbox("Select a university", sorted(university_data['University'].unique()))
+#     filtered_data_by_university = university_data[university_data['University'] == university_name]
+#     st.write(filtered_data_by_university)
+
+
 def main():
     st.set_page_config(page_title="University Recruitment Info", layout="wide")
     st.title("University Recruitment Info")
 
-    st.header("Companies and Universities")
+    if "search_started" not in st.session_state:
+        st.session_state.search_started = False
 
-    university_data = get_university_data()
-    st.write(university_data)
-
-    st.header("Filter by Company")
-    company_name = st.selectbox("Select a company", sorted(university_data['Company'].unique()))
-    filtered_data = university_data[university_data['Company'] == company_name]
-    st.write(filtered_data)
-
-    st.header("Filter by University")
-    university_name = st.selectbox("Select a university", sorted(university_data['University'].unique()))
-    filtered_data_by_university = university_data[university_data['University'] == university_name]
-    st.write(filtered_data_by_university)
+    university_data = pd.DataFrame()  # Initialize university_data as an empty DataFrame
 
     st.header("Reset Data")
     reset_button = st.button("Reset")
+    run_button = st.button("Run")
 
     if reset_button:
         csv_folder = 'csv'
@@ -156,6 +160,23 @@ def main():
             if filename.endswith('.csv'):
                 os.remove(f'{csv_folder}/{filename}')
         st.success("All CSV files have been cleared.")
-        st.experimental_rerun()
+        st.session_state.search_started = False
+
+    if run_button:
+        st.session_state.search_started = True
+        university_data = get_university_data()
+
+    if st.session_state.search_started:
+        filter_company_placeholder = st.empty()
+        filter_company_placeholder.header("Filter by Company")
+        company_name = filter_company_placeholder.selectbox("Select a company", sorted(university_data['Company'].unique()))
+        filtered_data = university_data[university_data['Company'] == company_name]
+        filter_company_placeholder.write(filtered_data)
+
+        filter_university_placeholder = st.empty()
+        filter_university_placeholder.header("Filter by University")
+        university_name = filter_university_placeholder.selectbox("Select a university", sorted(university_data['University'].unique()))
+        filtered_data_by_university = university_data[university_data['University'] == university_name]
+        filter_university_placeholder.write(filtered_data_by_university)
 
 main()
